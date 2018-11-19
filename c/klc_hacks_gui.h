046e2db894cc3060ea60bba4158acbd91f2d4f7b
Kyumin Kim <math4tots@users.noreply.github.com>
2018-11-18 22:14:57 -0800

    hacks.gui draw text

--- c/klc_hacks_gui.h
+++ c/klc_hacks_gui.h
@@ -5,16 +5,17 @@
 
 typedef struct KLCNhacksZBguiZBApi KLCNhacksZBguiZBApi;
 typedef struct KLCNhacksZBguiZBGraphicsContext KLCNhacksZBguiZBGraphicsContext;
+typedef struct KLCNhacksZBguiZBFont KLCNhacksZBguiZBFont;
 struct KLCNhacksZBguiZBOptions;
 struct KLCNhacksZBguiZBColor;
 
 extern KLC_typeinfo KLC_typehacksZBguiZBApi;
 extern KLC_typeinfo KLC_typehacksZBguiZBGraphicsContext;
-extern KLC_typeinfo KLC_typehacksZBguiZBColor;
+extern KLC_typeinfo KLC_typehacksZBguiZBFont;
 
 void KLC_deletehacksZBguiZBApi(KLC_header*, KLC_header**);
 void KLC_deletehacksZBguiZBGraphicsContext(KLC_header*, KLC_header**);
-void KLC_deletehacksZBguiZBColor(KLC_header*, KLC_header**);
+void KLC_deletehacksZBguiZBFont(KLC_header*, KLC_header**);
 
 KLCNTry* KLCNhacksZBguiZBtryApiZEinit();
 
@@ -23,12 +24,32 @@ void KLCNhacksZBguiZBApiZFstart(KLCNhacksZBguiZBApi* api, struct KLCNhacksZBguiZ
 
 double KLCNhacksZBguiZBGraphicsContextZFGETwidth(KLCNhacksZBguiZBGraphicsContext* gc);
 double KLCNhacksZBguiZBGraphicsContextZFGETheight(KLCNhacksZBguiZBGraphicsContext* gc);
+KLCNList* KLCNhacksZBguiZBGraphicsContextZFfillTextSize(
+  KLCNhacksZBguiZBGraphicsContext* gc,
+  KLCNString* text);
 void KLCNhacksZBguiZBGraphicsContextZFsetFillColor(
+  KLCNhacksZBguiZBGraphicsContext* gc,
+  struct KLCNhacksZBguiZBColor* color);
+void KLCNhacksZBguiZBGraphicsContextZFsetFont(
+  KLCNhacksZBguiZBGraphicsContext* gc,
+  KLCNhacksZBguiZBFont* font);
+void KLCNhacksZBguiZBGraphicsContextZFsetTextForegroundColor(
+  KLCNhacksZBguiZBGraphicsContext* gc,
+  struct KLCNhacksZBguiZBColor* color);
+void KLCNhacksZBguiZBGraphicsContextZFsetTextBackgroundColor(
   KLCNhacksZBguiZBGraphicsContext* gc,
   struct KLCNhacksZBguiZBColor* color);
 void KLCNhacksZBguiZBGraphicsContextZFfillRect(
   KLCNhacksZBguiZBGraphicsContext* gc,
   double x, double y, double width, double height);
+void KLCNhacksZBguiZBGraphicsContextZFfillText(
+  KLCNhacksZBguiZBGraphicsContext* gc,
+  double x, double y, KLCNString* text);
+
+KLCNString* KLCNhacksZBguiZBFontZFGETname(KLCNhacksZBguiZBFont* font);
+KLC_int KLCNhacksZBguiZBFontZFGETsize(KLCNhacksZBguiZBFont* font);
+
+KLCNTry* KLCNhacksZBguiZBgetFontZDtry(KLCNString* name, KLC_int size);
 
 /* BEGIN Function declarations for extracting data from Options */
 KLC_int KLCNhacksZBguiZBOptionsZFGETx(struct KLCNhacksZBguiZBOptions* opts);
--- c/klc_hacks_gui_osx.m
+++ c/klc_hacks_gui_osx.m
@@ -4,6 +4,7 @@
 typedef struct KLCNhacksZBguiZBOptions Options;
 typedef struct KLCNhacksZBguiZBGraphicsContext GC;
 typedef struct KLCNhacksZBguiZBColor Color;
+typedef struct KLCNhacksZBguiZBFont Font;
 
 /* NOTE: ARC is assumed/required here */
 
@@ -12,6 +13,7 @@ typedef struct KLCNhacksZBguiZBColor Color;
 
 /* TODO: Once ARC support for object pointers in structs is */
 /* more widely available, remove all the '__unsafe_unretained' */
+/* and stop using retainForever */
 
 @class KLCOBJCView;
 @class KLCOBJCAppDelegate;
@@ -29,6 +31,17 @@ struct KLCNhacksZBguiZBGraphicsContext {
   /* of the call. So they should never outlive the view that */
   /* spawned them */
   __unsafe_unretained KLCOBJCView* view;
+
+  Font* font;
+  Color* textForegroundColor;
+  Color* textBackgroundColor;
+};
+
+struct KLCNhacksZBguiZBFont {
+  KLC_header header;
+
+  /* held with retainForever */
+  __unsafe_unretained NSFont* font;
 };
 
 @interface KLCOBJCView: NSView {
@@ -43,19 +56,59 @@ struct KLCNhacksZBguiZBGraphicsContext {
 }
 @end
 
+static NSMutableArray<NSObject*>* retainArray = nil;
+
+static NSMutableArray<NSObject*>* getRetainSet() {
+  if (retainArray == nil) {
+    retainArray = [[NSMutableArray<NSObject*> alloc] init];
+  }
+  return retainArray;
+}
+
+static void retainForever(NSObject* obj) {
+  [getRetainSet() addObject: obj];
+}
+
+static Font* mkfont(NSFont* font) {
+  Font* ret = (Font*) malloc(sizeof(Font));
+  KLC_init_header((KLC_header*) ret, &KLC_typehacksZBguiZBFont);
+  ret->font = font;
+  retainForever(font);
+  return ret;
+}
+
 static GC* makeGC(KLCOBJCView* view) {
   GC* gc = (GC*) malloc(sizeof(GC));
   KLC_init_header((KLC_header*) gc, &KLC_typehacksZBguiZBGraphicsContext);
   gc->view = view;
+  gc->font = NULL;
+  gc->textForegroundColor = NULL;
+  gc->textBackgroundColor = NULL;
   return gc;
 }
 
-static NSColor* convertColor(Color* color) {
+static NSColor* toNSColor(Color* color) {
   return [NSColor
     colorWithRed: KLCNhacksZBguiZBColorZFGETr(color)
     green: KLCNhacksZBguiZBColorZFGETg(color)
     blue: KLCNhacksZBguiZBColorZFGETb(color)
     alpha: KLCNhacksZBguiZBColorZFGETa(color)];
+}
+
+static NSDictionary* getDrawTextAttributes(KLCNhacksZBguiZBGraphicsContext* gc) {
+  NSMutableDictionary* attrs = [[NSMutableDictionary alloc] init];
+  [attrs setValue: gc->font->font forKey: NSFontAttributeName];
+  if (gc->textForegroundColor) {
+    [attrs
+      setValue: toNSColor(gc->textForegroundColor)
+      forKey: NSForegroundColorAttributeName];
+  }
+  if (gc->textBackgroundColor) {
+    [attrs
+      setValue: toNSColor(gc->textBackgroundColor)
+      forKey: NSBackgroundColorAttributeName];
+  }
+  return attrs;
 }
 
 @implementation KLCOBJCView: NSView
@@ -66,19 +119,21 @@ static NSColor* convertColor(Color* color) {
   return self;
 }
 - (void)drawRect:(NSRect)dirtyRect {
-  /* NOTE: There's an implicit assumption here that bounds.origin is always (0, 0) */
-  NSRect bounds = self.bounds;
-  GC* gc = makeGC(self);
-  KLC_var drawCallback = KLCNhacksZBguiZBOptionsZFGETdrawCallback(appDelegate->opts);
-  if (KLC_truthy(drawCallback)) {
-    KLC_var gcvar = KLC_object_to_var((KLC_header*) gc);
-    KLC_release_var(KLC_var_call(drawCallback, 1, &gcvar));
+  @autoreleasepool {
+    /* NOTE: There's an implicit assumption here that bounds.origin is always (0, 0) */
+    NSRect bounds = self.bounds;
+    GC* gc = makeGC(self);
+    KLC_var drawCallback = KLCNhacksZBguiZBOptionsZFGETdrawCallback(appDelegate->opts);
+    if (KLC_truthy(drawCallback)) {
+      KLC_var gcvar = KLC_object_to_var((KLC_header*) gc);
+      KLC_release_var(KLC_var_call(drawCallback, 1, &gcvar));
+    }
+    if (gc->header.refcnt) {
+      KLC_errorf("Retaining GraphicsContext outside onDraw callback is illegal");
+    }
+    KLC_release_var(drawCallback);
+    KLC_release((KLC_header*) gc);
   }
-  if (gc->header.refcnt) {
-    KLC_errorf("Retaining GraphicsContext outside onDraw callback is illegal");
-  }
-  KLC_release_var(drawCallback);
-  KLC_release((KLC_header*) gc);
 }
 @end
 
@@ -114,7 +169,16 @@ static NSColor* convertColor(Color* color) {
 void KLC_deletehacksZBguiZBApi(KLC_header* api, KLC_header** dq) {
 }
 
-void KLC_deletehacksZBguiZBGraphicsContext(KLC_header* api, KLC_header** dq) {
+void KLC_deletehacksZBguiZBGraphicsContext(KLC_header* robj, KLC_header** dq) {
+  KLCNhacksZBguiZBGraphicsContext* gc = (KLCNhacksZBguiZBGraphicsContext*) robj;
+  KLC_partial_release((KLC_header*) gc->font, dq);
+}
+
+void KLC_deletehacksZBguiZBFont(KLC_header* robj, KLC_header** dq) {
+  @autoreleasepool {
+    KLCNhacksZBguiZBFont* font = (KLCNhacksZBguiZBFont*) robj;
+    /* nsrelease(font->font); */
+  }
 }
 
 KLCNTry* KLCNhacksZBguiZBtryApiZEinit() {
@@ -171,13 +235,80 @@ double KLCNhacksZBguiZBGraphicsContextZFGETheight(KLCNhacksZBguiZBGraphicsContex
   return gc->view.bounds.size.height;
 }
 
+KLCNList* KLCNhacksZBguiZBGraphicsContextZFfillTextSize(
+    KLCNhacksZBguiZBGraphicsContext* gc,
+    KLCNString* text) {
+  NSString* nstext = [NSString stringWithUTF8String: text->utf8];
+  NSSize size = [nstext sizeWithAttributes: getDrawTextAttributes(gc)];
+  KLCNList* ret = KLC_mklist(2);
+  KLCNListZFpush(ret, KLC_double_to_var(size.width));
+  KLCNListZFpush(ret, KLC_double_to_var(size.height));
+  return ret;
+}
+
 void KLCNhacksZBguiZBGraphicsContextZFsetFillColor(
     KLCNhacksZBguiZBGraphicsContext* gc, Color* color) {
-  [convertColor(color) setFill];
+  [toNSColor(color) setFill];
+}
+
+void KLCNhacksZBguiZBGraphicsContextZFsetFont(
+    KLCNhacksZBguiZBGraphicsContext* gc, Font* font) {
+  KLC_retain((KLC_header*) font);
+  KLC_release((KLC_header*) gc->font);
+  gc->font = font;
+}
+
+void KLCNhacksZBguiZBGraphicsContextZFsetTextForegroundColor(
+    KLCNhacksZBguiZBGraphicsContext* gc, Color* color) {
+  KLC_retain((KLC_header*) color);
+  KLC_release((KLC_header*) gc->textForegroundColor);
+  gc->textForegroundColor = color;
+}
+
+void KLCNhacksZBguiZBGraphicsContextZFsetTextBackgroundColor(
+    KLCNhacksZBguiZBGraphicsContext* gc, Color* color) {
+  KLC_retain((KLC_header*) color);
+  KLC_release((KLC_header*) gc->textBackgroundColor);
+  gc->textBackgroundColor = color;
 }
 
 void KLCNhacksZBguiZBGraphicsContextZFfillRect(
     KLCNhacksZBguiZBGraphicsContext* gc,
     double x, double y, double width, double height) {
-  NSRectFill(NSMakeRect(x, y, width, height));
+  @autoreleasepool {
+    NSRectFill(NSMakeRect(x, y, width, height));
+  }
+}
+
+void KLCNhacksZBguiZBGraphicsContextZFfillText(
+    KLCNhacksZBguiZBGraphicsContext* gc,
+    double x, double y, KLCNString* text) {
+  @autoreleasepool {
+    NSString* nstext = [NSString stringWithUTF8String: text->utf8];
+    [nstext
+      drawAtPoint: NSMakePoint(x, y)
+      withAttributes:getDrawTextAttributes(gc)];
+  }
+}
+
+KLCNString* KLCNhacksZBguiZBFontZFGETname(KLCNhacksZBguiZBFont* font) {
+  return KLC_mkstr(font->font.fontName.UTF8String);
+}
+
+KLC_int KLCNhacksZBguiZBFontZFGETsize(KLCNhacksZBguiZBFont* font) {
+  return (KLC_int) font->font.pointSize;
+}
+
+KLCNTry* KLCNhacksZBguiZBgetFontZDtry(KLCNString* name, KLC_int size) {
+  @autoreleasepool {
+    NSString* nsname = [NSString stringWithUTF8String: name->utf8];
+    NSFont* nsfont = [NSFont fontWithName: nsname size: size];
+    if (nsfont) {
+      Font* font = mkfont(nsfont);
+      KLCNTry* ret = KLCNTryZEnew(1, KLC_object_to_var((KLC_header*) font));
+      KLC_release((KLC_header*) font);
+      return ret;
+    }
+    return KLC_failm("Could not find font with given name");
+  }
 }
