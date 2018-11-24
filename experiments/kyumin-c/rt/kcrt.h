065175b31fc8083bf76e2a825f16bc8005d43ce3
Kyumin Kim <math4tots@users.noreply.github.com>
2018-11-24 01:07:18 -0800

    ...

--- experiments/kyumin-c/rt/kcrt.h
+++ experiments/kyumin-c/rt/kcrt.h
@@ -5,9 +5,22 @@
 #include <limits.h>
 #include <stddef.h>
 
+#define KLC_TAG_POINTER 0
+#define KLC_TAG_BOOL 1
+#define KLC_TAG_INT 2
+#define KLC_TAG_FLOAT 3
+
+#if defined(LLONG_MAX)
+typedef long long KLCint;
+#define KLC_INT_FMT "%lld"
+#define KLC_INT_MAX LLONG_MAX
+#define KLC_INT_MIN LLONG_MIN
+#else
 typedef long KLCint;
 #define KLC_INT_FMT "%ld"
 #define KLC_INT_MAX LONG_MAX
+#define KLC_INT_MIN LONG_MIN
+#endif
 
 /* TODO: Also compare with PTRDIFF_MAX */
 #if KLC_INT_MAX <= 2147483647L
@@ -16,13 +29,40 @@ typedef long KLCint;
 
 typedef double KLCfloat;
 typedef struct KLCvar KLCvar;
+typedef struct KLCheader KLCheader;
+typedef struct KLCXClass KLCXClass;
+typedef struct KLCXAutoReleasePool KLCXAutoReleasePool;
+typedef KLCvar KLCXMethod(KLCvar, int, KLCvar*);
 
 struct KLCvar {
   int tag;
   union {
+    KLCheader* p;
     KLCint i;
     KLCfloat f;
   } u;
 };
+
+struct KLCheader {
+  size_t refcnt;
+  KLCXClass* cls;
+};
+
+struct KLCXAutoReleasePool {
+  size_t size, cap;
+  KLCvar* buffer;
+};
+
+/* OOP utilities */
+KLCXClass* KLCXGetClass(KLCvar);
+const char* KLCXGetClassName(KLCXClass*);
+KLCXMethod* KLCXGetMethodForClass(KLCXClass*, const char*);
+KLCvar KLCXCallMethod(KLCvar, const char*, int, ...);
+KLCXClass* KLCXNewClass(const char*);
+KLCvar KLCXAddMethod(KLCXClass*, const char*, KLCXMethod*);
+
+/* Reference counting utilities */
+KLCvar KLCXPush(KLCXAutoReleasePool*, KLCvar);
+void KLCXResize(KLCXAutoReleasePool*, size_t);
 
 #endif/*KCRT_H*/
